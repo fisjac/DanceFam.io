6bbc213520e64f00c605d0d19656e0453c9196ef
fisjac <jfisher9882@gmail.com>
2022-10-31 09:39:08 -0700

    communities store

---
+++ react-app/src/store/communities.js
@@ -0,0 +1,99 @@
+const LOAD_COMMUNITIES = 'communities/LOAD_ALL';
+const LOAD_COMMUNITY = 'communities/LOAD_ONE';
+const EDIT = 'communities/EDIT';
+const DELETE = 'communities/DELETE';
+
+
+// Actions
+export function load_communities (payload) {
+  return {
+    type: LOAD_COMMUNITIES,
+    payload
+  }
+};
+
+export function load_community (payload) {
+  return {
+    type: LOAD_COMMUNITY,
+    payload
+  }
+};
+
+export function edit_community (payload) {
+  return {
+    type: EDIT,
+    payload
+  }
+};
+
+export function delete_community (payload) {
+  return {
+    type: DELETE,
+    payload
+  }
+};
+
+// Thunks
+
+export const get_communities = () => async dispatch => {
+  const response = await fetch('/api/communities');
+  if (response.ok) {
+    const communities = await response.json();
+    dispatch(load_communities(communities));
+    return response;
+  };
+  return response;
+};
+
+export const get_community = (communityId) => async dispatch => {
+  const response = await fetch(`/api/communities/${communityId}`);
+  if (response.ok) {
+    const community = await response.json();
+    dispatch(load_community(community));
+    return response;
+  };
+  return response;
+};
+
+export const edit_community = (community) => async dispatch => {
+  const response = await fetch(`/api/communities/${community.id}`,{
+    method: 'PUT',
+    header: {'Content-Type': 'application/json'},
+    body: community
+  });
+  if (response.ok) {
+    const community = await response.json();
+    dispatch(edit_community(community));
+    return response;
+  };
+  return response;
+};
+
+export const delete_community = (communityId) => async dispatch => {
+  const response = await fetch (`api/communities/${communityId}`,{
+    method: 'DELETE'
+  });
+  if (response.ok) {
+    dispatch(delete_community(communityId))
+    return response;
+  };
+  return response;
+};
+
+
+initalState = {communities: null, singleCommunity: null};
+
+export default function communitiesReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOAD_COMMUNITIES:
+      return;
+    case LOAD_COMMUNITY:
+      return;
+    case EDIT:
+      return;
+    case DELETE:
+      return;
+    default:
+      return state;
+  }
+}
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -99,7 +99,7 @@ export const signUp = (firstName, lastName, username, email, password) => async 
   }
 }
 
-export default function reducer(state = initialState, action) {
+export default function sessionReducer(state = initialState, action) {
   switch (action.type) {
     case SET_USER:
       return { user: action.payload }
