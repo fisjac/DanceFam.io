855ea04c263ffdac0cb133f7a7d9c03a1be8af8e
fisjac <jfisher9882@gmail.com>
2022-11-04 11:47:24 -0500

    removing slashes from event and community routes

--- app/api/community_routes.py
+++ app/api/community_routes.py
@@ -18,7 +18,7 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 # GET all
-@community_routes.route('/')
+@community_routes.route('')
 def communities():
     communities = Community.query.all()
     return jsonify([community.to_dict() for community in communities])
