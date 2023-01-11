35cb173c97236cd2fda08f2e736ad3e83e1b8fe6
fisjac <jfisher9882@gmail.com>
2023-01-11 13:56:41 -0600

    commented out autocomplete

--- react-app/src/components/Browser/forms/CreateEventForm.js
+++ react-app/src/components/Browser/forms/CreateEventForm.js
@@ -7,46 +7,46 @@ import { getKey } from '../../../store/keys';
 
 let autoComplete;
 
-const loadScript = (url, callback) => {
-  let script = document.createElement("script"); // create script tag
-  script.type = "text/javascript";
+// const loadScript = (url, callback) => {
+//   let script = document.createElement("script"); // create script tag
+//   script.type = "text/javascript";
 
-  // when script state is ready and loaded or complete we will call callback
-  if (script.readyState) {
-    script.onreadystatechange = function() {
-      if (script.readyState === "loaded" || script.readyState === "complete") {
-        script.onreadystatechange = null;
-        callback();
-      }
-    };
-  } else {
-    script.onload = () => callback();
-  }
+//   // when script state is ready and loaded or complete we will call callback
+//   if (script.readyState) {
+//     script.onreadystatechange = function() {
+//       if (script.readyState === "loaded" || script.readyState === "complete") {
+//         script.onreadystatechange = null;
+//         callback();
+//       }
+//     };
+//   } else {
+//     script.onload = () => callback();
+//   }
 
-  script.src = url; // load by url
-  document.getElementsByTagName("head")[0].appendChild(script); // append to head
-};
+//   script.src = url; // load by url
+//   document.getElementsByTagName("head")[0].appendChild(script); // append to head
+// };
 
-// handle when the script is loaded we will assign autoCompleteRef with google maps place autocomplete
-function handleScriptLoad(updateQuery, autoCompleteRef) {
-  // assign autoComplete with Google maps place one time
-  autoComplete = new window.google.maps.places.Autocomplete(
-    autoCompleteRef.current,
-    {}
-  );
-  autoComplete.setFields(["address_components", "formatted_address"]); // specify what properties we will get from API
-  // add a listener to handle when the place is selected
-  autoComplete.addListener("place_changed", () =>
-    handlePlaceSelect(updateQuery)
-  );
-}
+// // handle when the script is loaded we will assign autoCompleteRef with google maps place autocomplete
+// function handleScriptLoad(updateQuery, autoCompleteRef) {
+//   // assign autoComplete with Google maps place one time
+//   autoComplete = new window.google.maps.places.Autocomplete(
+//     autoCompleteRef.current,
+//     {}
+//   );
+//   autoComplete.setFields(["address_components", "formatted_address"]); // specify what properties we will get from API
+//   // add a listener to handle when the place is selected
+//   autoComplete.addListener("place_changed", () =>
+//     handlePlaceSelect(updateQuery)
+//   );
+// }
 
-async function handlePlaceSelect(updateQuery) {
-  const addressObject = autoComplete.getPlace();
-  const query = addressObject.formatted_address;
-  updateQuery(query);
-  console.log(addressObject);
-}
+// async function handlePlaceSelect(updateQuery) {
+//   const addressObject = autoComplete.getPlace();
+//   const query = addressObject.formatted_address;
+//   updateQuery(query);
+//   console.log(addressObject);
+// }
 
 export default function CreateEventForm({setShowModal}) {
   const dispatch = useDispatch();
@@ -68,16 +68,16 @@ export default function CreateEventForm({setShowModal}) {
   const [imageUrl, setImageUrl] = useState(null);
   const key = useSelector(state => state.keys.places);
 
-  console.log('places key', key)
-  // Google Maps autocomplete API script
-  useEffect(async () => {
-      await dispatch(getKey('places'))
-      console.log('places key in useEffect', key)
-      loadScript(
-        `https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`,
-        () => handleScriptLoad(setAddress, autoCompleteRef)
-      );
-    }, [dispatch, key]);
+  // console.log('places key', key)
+  // // Google Maps autocomplete API script
+  // useEffect(async () => {
+  //     await dispatch(getKey('places'))
+  //     console.log('places key in useEffect', key)
+  //     loadScript(
+  //       `https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`,
+  //       () => handleScriptLoad(setAddress, autoCompleteRef)
+  //     );
+  //   }, [dispatch, key]);
 
 
 
