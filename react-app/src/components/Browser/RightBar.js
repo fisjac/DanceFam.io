b1ae8cec031730188835a7a7fa10cec507b2c231
fisjac <jfisher9882@gmail.com>
2023-02-02 14:42:06 -0600

    removed right banner pic

--- react-app/src/components/Browser/RightBar.js
+++ react-app/src/components/Browser/RightBar.js
@@ -4,7 +4,7 @@ import linkedin from '../../static/linkedin.png';
 export default function RightBar() {
   return (
     <div className='right-bar'>
-      <a
+      {/* <a
         className='banner-ad'
         href='https://www.linkedin.com/in/jackson-fisher-8631a7a1/'
         style={{backgroundImage: `url(${profPic})`}}
@@ -14,7 +14,7 @@ export default function RightBar() {
           <div className='linkedin-text'>Looking for world-class developer to join your team?</div>
 
         </div>
-      </a>
+      </a> */}
     </div>
   )
 }
