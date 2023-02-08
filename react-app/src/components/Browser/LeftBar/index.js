f98cba1df8541ddbdd4746a0b5f67b8353911ce4
fisjac <jfisher9882@gmail.com>
2023-02-08 15:57:53 -0600

    added localstorage for filters

--- react-app/src/components/Browser/LeftBar/index.js
+++ react-app/src/components/Browser/LeftBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
@@ -9,68 +9,14 @@ import defaultImage from '../../../static/dancing_couple1.svg'
 import { toggleStyle } from '../../../store/styles';
 import { toggleType } from '../../../store/types';
 
-
-function NextEvent () {
-  const user = useSelector(state=>state.session.user);
-  const events = useSelector(state=>state.events)
-  const history = useHistory();
-
-  const userEvents = Object.keys(user.events).map(eventId=>events[eventId]);
-
-  const currentUserEvents = userEvents.filter(event =>{
-    const currentEventDate = new Date(event.start);
-    const today = new Date();
-    return currentEventDate > today;
-  })
-
-  if (Object.keys(currentUserEvents).length) {
-    const nextEvent = currentUserEvents.reduce((accum,event)=> {
-      const currentEventDate = new Date(event.start);
-      const currentMinDate = new Date(accum.start);
-      return currentEventDate < currentMinDate ? event : accum
-    })
-
-    const start = new Date(nextEvent.start);
-    return (
-      <div
-        className='event-box'
-        onClick={()=>history.push(`/events/${nextEvent.id}`)}>
-          <div className='event-box-header'>
-            <img
-              className='event-box-image'
-              src={nextEvent.imageUrl===null? defaultImage :nextEvent.imageUrl}
-              alt="event_image"
-              onError={e =>e.currentTarget.src = defaultImage}
-              />
-            <div className='event-box-title'>{nextEvent.name}</div>
-          </div>
-          <div className='event-box-details'>
-            <div className='event-box-date'>{start.toLocaleDateString(undefined, {weekday: 'short', month: 'short', day: 'numeric',})} ⋅ {start.toLocaleTimeString(undefined, {timeStyle: 'short'})}</div>
-            <div className='event-box-address-line'>
-              <div className='location-icon'>
-                <i className="fa-solid fa-location-dot"></i>
-              </div>
-              <div className='address-section'>
-                <div className='event-box-address'>{nextEvent.address}</div>
-                  <div className='city-state'>
-                    <div className='event-box-city'>{`${nextEvent.city}, ${nextEvent.state}`}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-    )
-  } else {
-    return (
-      <div className='event-box-none'>You have no upcoming events</div>
-    )
-  }
-}
-
-
 export default function LeftBar({types, styles}) {
   const history = useHistory();
   const dispatch = useDispatch();
+
+  useEffect(()=> {
+    localStorage.setItem('types',JSON.stringify(types))
+    localStorage.setItem('styles',JSON.stringify(styles))
+  },[types,styles])
 
   return (
     <div className='left-bar'>
